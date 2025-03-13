81a1df76f99a35542d655d74b4a7c4cf0df23793
Vaidehi106 <patelvaidehi1006@gmail.com>
2025-03-13 15:08:33 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+var1=int(input("enter first number: "))
+var2=int(input("enter second number: "))
+
+add=var1+var2
+print("Addition: ",add)
+
+sub=var1-var2
+print("substraction: ",sub)
+
+mul=var1*var2
+print("multiply: ",mul)
+
+div=var1/var2
+print("division: ",div)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+f_name=input("enter your first name: ")
+l_name=input("enter your last name: ")
+
+print (f"hello,{f_name} {l_name}! welcome to the python program")
